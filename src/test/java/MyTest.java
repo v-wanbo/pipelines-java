bbfe683c8ae68e26bd1dbe063019ebae598171e9
v-wanbo <49471307+v-wanbo@users.noreply.github.com>
2019-04-18 09:52:55 +0800

    Update MyTest.java

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -2,12 +2,13 @@ import com.microsoft.demo.Demo;
 import org.junit.Test;
 
 public class MyTest {
+    /* 2019/04/18 注释 test,project */
     @Test
     public void test_method_1() {
         Demo d = new Demo();
         d.DoSomething(true);
     }
-
+    
     @Test
     public void test_method_2() {
     }
